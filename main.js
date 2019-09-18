dd8e2750abf47a01f7f7b9084f88999bec3dac85
Artem Belyavskiy <artembell305@gmail.com>
2019-09-18 22:51:03 +0300

    refactor: add max window and get react-bootstrap

--- main.js
+++ main.js
@@ -3,20 +3,14 @@ const {
     BrowserWindow
 } = require('electron')
 
-console.log("ELECTRON RUN");
-
 function createWindow() {
-    // Create the browser window.
     let win = new BrowserWindow({
-        width: 800,
-        height: 600,
         webPreferences: {
             nodeIntegration: true
         }
     })
-
+    win.maximize();
     win.loadURL('http://localhost:9000');
-    //   win.loadFile('./dist/index.html');
 
     win.webContents.openDevTools();
 
