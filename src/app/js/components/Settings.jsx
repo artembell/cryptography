862dafb61f1e10e6fa2316ef2d25ddc58a9e9382
Artem Belyavskiy <artembell305@gmail.com>
2019-09-21 16:41:56 +0300

    feature: add all controls and events

--- src/app/js/components/Settings.jsx
+++ src/app/js/components/Settings.jsx
@@ -1,40 +1,92 @@
 import React from "react";
-import { Form, FormGroup, FormLabel, FormControl } from "react-bootstrap";
+import { Form, FormGroup, FormLabel, FormControl, Row, Col, Button, ButtonToolbar } from "react-bootstrap";
+import {Ciphers} from '../ciphers/index';
+import CipherNames from '../enums/CipherNames';
 
 export default class Settings extends React.Component {
 	constructor(props) {
 		super(props);
 
+		this.availableCiphers = Object.values(CipherNames)
+
 		this.state = {
-			currentCipher: 0
+			currentCipherIndex: 0,
+			plainText: '123',
+			cipherText: 'asd'
 		};
 	}
+
+	onCipherChange(e) {
+		
+		const index = this.availableCiphers.findIndex(cipher => {
+			return cipher === e.target.value;
+		});
+		this.setState({ currentCipherIndex: index });
+	}
+
+	onEncipherClick(e){
+		const cipherText = Ciphers[this.state.currentCipherIndex].encipher(this.state.plainText);
+		this.setState({cipherText});
+	}
+
+	onDecipherClick(e){
+		const plainText = Ciphers[this.state.currentCipherIndex].decipher(this.state.cipherText);
+		this.setState({plainText});
+	}
+
 	render() {
 		return (
 			<Form>
-				<Form.Group controlId="exampleForm.ControlInput1">
-					<Form.Label>Choose file1</Form.Label>
-					<Form.Control type="file"/>
-				</Form.Group>
-                <Form.Group controlId="exampleForm.ControlInput2">
-					<Form.Label>Choose file2</Form.Label>
-					<Form.Control type="file"/>
-				</Form.Group>
-				<Form.Group controlId="exampleForm.ControlSelect1">
-					<Form.Label>Example select</Form.Label>
-					<Form.Control as="select">
-						<option>1</option>
-						<option>2</option>
-						<option>3</option>
-					</Form.Control>
-				</Form.Group>
+				<Row>
+					<Col md={4}>
+						<Form.Label>Choose ciphers</Form.Label>
+						<Form.Control
+							onChange={e => this.onCipherChange(e)}
+							value={this.availableCiphers[this.state.currentCipherIndex]}
+							as="select"
+						>
+							{this.availableCiphers.map((cipher, index) => {
+								return (
+									<option key={index} value={cipher}>
+										{cipher}
+									</option>
+								);
+							})}
+						</Form.Control>
+					</Col>
+					<Col md={4}>
+						<Form.Label>Choose input file</Form.Label>
+						<Form.Control type="file" />
+					</Col>
+					<Col md={4}>
+						<Form.Label>Choose output file</Form.Label>
+						<Form.Control type="file" />
+					</Col>					
+				</Row>
+
+				<Row>
+					<Col>
+						<Form.Label>Key</Form.Label>
+						<Form.Control type="text" placeholder="Enter key" />
+						<Form.Text className="text-muted">
+							This key must contain letters only! (or not)
+						</Form.Text>
+					</Col>
+					<Col>
+						<ButtonToolbar>
+							<Button onClick={(e) => this.onEncipherClick(e)} variant="success" size="lg">Encipher</Button>
+							<Button onClick={(e) => this.onDecipherClick(e)} variant="warning" size="lg">Decipher</Button>
+						</ButtonToolbar>
+					</Col>
+				</Row>
+
 				<Form.Group controlId="exampleForm.ControlTextarea1">
 					<Form.Label>Example textarea1</Form.Label>
-					<Form.Control as="textarea" rows="3" />
+					<Form.Control as="textarea" rows="3" value={this.state.plainText}/>
 				</Form.Group>
 				<Form.Group controlId="exampleForm.ControlTextarea2">
 					<Form.Label>Example textarea2</Form.Label>
-					<Form.Control as="textarea" rows="3" />
+					<Form.Control as="textarea" rows="3" value={this.state.cipherText}/>
 				</Form.Group>
 			</Form>
 		);
