527ca33a94157a9d6d9149d7b9fca4958f888c41
Artem Belyavskiy <artembell305@gmail.com>
2019-09-26 00:37:21 +0300

    test: railwat encipher

--- src/tests/ciphers.test.js
+++ src/tests/ciphers.test.js
@@ -1,5 +1,17 @@
 import {Ciphers} from '../app/js/ciphers/index'
 
-test('vigenere', () => {
-    expect(Ciphers[2].encipher({text: 'криптография', key: 'ключ'})).toBe('хьжжэъбзкажц');
+test('vigenere (encipher)', () => {
+    expect(Ciphers[2].encipher({text: 'криптография', key: 'ключ'})).toBe('хьжжэъбзкажц')
 });
+
+test('railway (encipher) with correct key', () => {
+    const railway = Ciphers[0]
+    // expect(railway.encipher({text: 'cryptography', key: '1'})).toBe('cryptography')
+    expect(railway.encipher({text: 'cryptography', key: '2'})).toBe('cytgahrporpy')
+    expect(railway.encipher({text: 'cryptography', key: '3'})).toBe('ctarporpyygh')
+    expect(railway.encipher({text: 'cryptography', key: '4'})).toBe('cgroryytahpp')
+    expect(railway.encipher({text: 'cryptography', key: '11'})).toBe('cryptograpyh')
+    expect(railway.encipher({text: 'cryptography', key: '12'})).toBe('cryptography')
+    expect(railway.encipher({text: 'cryptography', key: '13'})).toBe('cryptography')
+    expect(railway.encipher({text: 'cryptography', key: '14'})).toBe('cryptography')
+})
