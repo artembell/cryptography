a527c60da6d928e8defa1bda580917055c27faa5
Artem Belyavskiy <artembell305@gmail.com>
2019-09-26 00:45:46 +0300

    test: end railway encipher

--- src/tests/ciphers.test.js
+++ src/tests/ciphers.test.js
@@ -6,10 +6,11 @@ test('vigenere (encipher)', () => {
 
 test('railway (encipher) with correct key', () => {
     const railway = Ciphers[0]
-    // expect(railway.encipher({text: 'cryptography', key: '1'})).toBe('cryptography')
+    expect(railway.encipher({text: 'cryptography', key: '1'})).toBe('cryptography')
     expect(railway.encipher({text: 'cryptography', key: '2'})).toBe('cytgahrporpy')
     expect(railway.encipher({text: 'cryptography', key: '3'})).toBe('ctarporpyygh')
     expect(railway.encipher({text: 'cryptography', key: '4'})).toBe('cgroryytahpp')
+    // ...
     expect(railway.encipher({text: 'cryptography', key: '11'})).toBe('cryptograpyh')
     expect(railway.encipher({text: 'cryptography', key: '12'})).toBe('cryptography')
     expect(railway.encipher({text: 'cryptography', key: '13'})).toBe('cryptography')
