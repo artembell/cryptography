e92dd1378dbe0e8b9e43d544eb6fba717274419b
Artem Belyavskiy <artembell305@gmail.com>
2019-09-26 07:21:00 +0300

    test: added enciphers check

--- src/tests/ciphers.test.js
+++ src/tests/ciphers.test.js
@@ -16,3 +16,21 @@ test('railway (encipher) with correct key', () => {
     expect(railway.encipher({text: 'cryptography', key: '13'})).toBe('cryptography')
     expect(railway.encipher({text: 'cryptography', key: '14'})).toBe('cryptography')
 })
+
+// test('columnar (encipher) with correct key', () => {
+//     const railway = Ciphers[1]
+//     expect(railway.encipher({text: 'cryptography', key: '1'})).toBe('cryptography')
+// })
+
+test('vigenere (encipher) with correct key', () => {
+    const cipher = Ciphers[2]
+    expect(cipher.encipher({text: 'криптография', key: 'ключ'})).toBe('хьжжэъбзкажц')
+    expect(cipher.encipher({text: 'криптография', key: 'ключик'})).toBe('хьжжыщньюлсй')
+    // expect(cipher.encipher({text: 'криптография', key: 'ключ'})).toBe('ctarporpyygh')
+    // expect(cipher.encipher({text: 'криптография', key: 'ключ'})).toBe('cgroryytahpp')
+    // // ...
+    // expect(cipher.encipher({text: 'криптография', key: 'ключ'})).toBe('cryptograpyh')
+    // expect(cipher.encipher({text: 'криптография', key: 'ключ'})).toBe('cryptography')
+    // expect(cipher.encipher({text: 'криптография', key: 'ключ'})).toBe('cryptography')
+    // expect(cipher.encipher({text: 'криптография', key: 'ключ'})).toBe('cryptography')
+})
